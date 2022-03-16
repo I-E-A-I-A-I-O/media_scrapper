39df9433db280cf21eb78196f7b0739813e9512b
I-E-A-I-A-I-O <jesusdavid268@outlook.com>
2022-03-16 08:49:22 -0400

    service is back. fixed timeout

--- cnnSearch.py
+++ cnnSearch.py
@@ -26,10 +26,10 @@ def get_video_src(base: str):
     caps['chromeOptions'] = {
         "args": ["--headless", "--disable-dev-shm-usage", "--no-sandbox", "--disable-gpu", "--window-size=1920x1080"]
     }
-    #s = ChromeService(ChromeDriverManager().install())
-    wd = webdriver.Chrome(desired_capabilities=caps, executable_path=os.environ.get("CHROMEDRIVER_PATH"))
+    s = ChromeService(ChromeDriverManager().install())
+    wd = webdriver.Chrome(service=s, desired_capabilities=caps, executable_path=os.environ.get("CHROMEDRIVER_PATH"))
     wd.get(base)
-    WebDriverWait(wd, 70000).until(EC.visibility_of_element_located((By.XPATH, "//*[@title = \"Play\"]")))
+    WebDriverWait(wd, 70).until(EC.visibility_of_element_located((By.XPATH, "//*[@title = \"Play\"]")))
     
     try:
         button_element = wd.find_element(By.XPATH, primary)
