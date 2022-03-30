1c9439802e89b2b8e6df6e0859fb270a7010a3c4
I-E-A-I-A-I-O <jesusdavid268@outlook.com>
2022-03-30 14:08:09 -0400

    eval

--- server/helpers/websiteLoader.ts
+++ server/helpers/websiteLoader.ts
@@ -22,13 +22,8 @@ const instagramProcess = async (url: string, page: puppeteer.Page, logger: Fasti
     ]);
     await page.type('input[name="username"]', process.env.INSTA_USER!, { delay: 50 })
     await page.type('input[name="password"]', process.env.INSTA_PASS!, { delay: 50 })
-    const response = await Promise.all([
-        page.waitForNavigation(),
-        page.click('#loginForm > div > div:nth-child(3) > button')
-    ]);
-    /*await page.waitForFrame(async (frame) => {
-        return frame.url().includes('/onetap') || frame.url() === url
-    })*/
+    await page.$eval('#loginForm > div > div:nth-child(3) > button', el => (el as HTMLElement).click())
+    const response = await page.waitForNavigation()
     logger.info(response)
     logger.info(`Login success. Current URL ${page.url()}`)
 
