f801add120ec465c623f2e38fafc7757b3909dc9
I-E-A-I-A-I-O <jesusdavid268@outlook.com>
2022-03-30 11:53:34 -0400

    logging

--- server/helpers/websiteLoader.ts
+++ server/helpers/websiteLoader.ts
@@ -1,27 +1,43 @@
 import { FastifyLoggerInstance } from "fastify"
 import puppeteer from "puppeteer"
 
-const instagramProcess = async (url: string, page: puppeteer.Page): Promise<string | null> => {
-    if (page.url() === url) return await page.content()
+const instagramProcess = async (url: string, page: puppeteer.Page, logger: FastifyLoggerInstance): Promise<string | null> => {
+    logger.info(`Instagram page receive. User url: ${url} Puppeteer url: ${page.url()}`)
 
-    if (!page.url().includes('/login')) return null
+    if (page.url() === url) {
+        logger.info(`URL ${page.url()} were equals. Returning content`)
+        return await page.content()
+    }
 
+    if (!page.url().includes('/login')) {
+        logger.info(`Not a login page. Aborting`)
+        return null
+    }
+
+    logger.info(`Login in into instagram`)
     await page.waitForSelector('input[name="username"]')
     await page.type('input[name="username"]', process.env.INSTA_USER!)
     await page.type('input[name="password"]', process.env.INSTA_PASS!)
     await page.click('button[type="submit"]')
     await page.waitForSelector('button[class="sqdOP yWX7d    y3zKF     "]')
+    logger.info(`Login sucess`)
 
     if (page.url().includes('/onetap')) {
+        logger.info(`Cookie page loaded`)
         await page.click('button[class="sqdOP yWX7d    y3zKF     "]')
         await page.waitForSelector('video[class="tWeCl"]')
+        logger.info(`Post page reached`)
         return await page.content()
     }
     else if (page.url() === url) {
+        logger.info(`Post page reached`)
         await page.waitForSelector('video[class="tWeCl"]')
         return await page.content()
     }
-    else return null
+    else {
+        logger.info(`Couldn't load post. Aborting`)
+        return null
+    }
 }
 
 export const loadHTML = async (url: string, logger: FastifyLoggerInstance): Promise<string | null> => {
@@ -36,7 +52,7 @@ export const loadHTML = async (url: string, logger: FastifyLoggerInstance): Prom
             return null
         }
 
-        if (url.includes("instagram.com")) return await instagramProcess(url, page)
+        if (url.includes("instagram.com")) return await instagramProcess(url, page, logger)
 
         const content = await page.content()
         return content
